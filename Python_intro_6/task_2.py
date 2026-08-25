55013f5c0a71b0e338391e9eba4abfd2bb177768
TheDoriPig <pashkovskiy2003@gmail.com>
2026-08-25 12:08:26 +0300

    Fix task_2 and task_3

--- Python_intro_6/task_2.py
+++ Python_intro_6/task_2.py
@@ -1,4 +1,4 @@
-length_rectangle = int(input("Введите длинну прямоугольника: "))
+length_rectangle = int(input("Введите длину прямоугольника: "))
 width_rectangle = int(input("Введите ширину прямоугольника: "))
 area_rectangle = length_rectangle * width_rectangle
 print(f"Площадь прямоугольника: {area_rectangle}")
--- Python_intro_6/task_3.py
+++ Python_intro_6/task_3.py
@@ -1,3 +1,3 @@
-temperature_c = int(input("Введите температуру в градусах Цельсия: "))
+temperature_c = float(input("Введите температуру в градусах Цельсия: "))
 temperature_f = temperature_c * 9 / 5 + 32
 print(f"{temperature_c}°C это {temperature_f}°F")
